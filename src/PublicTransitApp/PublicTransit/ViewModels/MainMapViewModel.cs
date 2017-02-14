66bc9cfc848b72e469d1e1555e924e5958a9d7b9
Christopher Moravec <morehavoc@gmail.com>
2017-02-14 11:24:06 -0800

    Working App to Part 2.  Now contains a button to center the map to the user's location. (#1)

---
+++ src/PublicTransitApp/PublicTransit/ViewModels/MainMapViewModel.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="MainMapViewModel.cs" company="Moravec Labs, LLC">
+//     MIT License
+//
+//     Copyright (c) Moravec Labs, LLC.
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+// </copyright>
+
+namespace PublicTransit
+{
+    using System;
+    using Esri.ArcGISRuntime.Mapping;
+    using MoravecLabs.Atom;
+    using MoravecLabs.Infrastructure;
+
+    /// <summary>
+    /// Main map view model.
+    /// </summary>
+    public class MainMapViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PublicTransit.MainMapViewModel"/> class.
+        /// </summary>
+        public MainMapViewModel()
+        {
+            this.Map = new Atom<Map>(null, this, "Map");
+        }
+
+        /// <summary>
+        /// Gets the map.
+        /// </summary>
+        /// <value>The map.</value>
+        public Atom<Map> Map { get; private set; }
+
+        /// <summary>
+        /// Initialize this instance.
+        /// </summary>
+        public void Initialize()
+        {
+            this.Map.Value = MapUtilities.CreateStandardMap();
+        }
+    }
+}
---
+++ src/PublicTransitApp/iOS/AppDelegate.cs
@@ -0,0 +1,115 @@
+﻿// <copyright file="AppDelegate.cs" company="Moravec Labs, LLC">
+//     MIT License
+//
+//     Copyright (c) Moravec Labs, LLC.
+//
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+// </copyright>
+
+namespace PublicTransit.iOS
+{
+    using Foundation;
+    using UIKit;
+
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+
+    /// <summary>
+    /// App delegate.
+    /// </summary>
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
+
+        /// <summary>
+        /// Gets or sets the window.
+        /// </summary>
+        /// <value>The window.</value>
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Finisheds the launching.
+        /// </summary>
+        /// <returns><c>true</c>, if launching was finisheded, <c>false</c> otherwise.</returns>
+        /// <param name="application">Application reference.</param>
+        /// <param name="launchOptions">Launch options.</param>
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+            return true;
+        }
+
+        /// <summary>
+        /// Ons the resign activation.
+        /// </summary>
+        /// <param name="application">Application reference.</param>
+        public override void OnResignActivation(UIApplication application)
+        {
+            // Invoked when the application is about to move from active to inactive state.
+            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+            // or when the user quits the application and it begins the transition to the background state.
+            // Games should use this method to pause the game.
+        }
+
+        /// <summary>
+        /// Dids the enter background.
+        /// </summary>
+        /// <param name="application">Application reference.</param>
+        public override void DidEnterBackground(UIApplication application)
+        {
+            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
+            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
+        }
+
+        /// <summary>
+        /// Wills the enter foreground.
+        /// </summary>
+        /// <param name="application">Application reference.</param>
+        public override void WillEnterForeground(UIApplication application)
+        {
+            // Called as part of the transiton from background to active state.
+            // Here you can undo many of the changes made on entering the background.
+        }
+
+        /// <summary>
+        /// Ons the activated.
+        /// </summary>
+        /// <param name="application">Application reference.</param>
+        public override void OnActivated(UIApplication application)
+        {
+            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
+            // If the application was previously in the background, optionally refresh the user interface.
+        }
+
+        /// <summary>
+        /// Wills the terminate.
+        /// </summary>
+        /// <param name="application">Application reference.</param>
+        public override void WillTerminate(UIApplication application)
+        {
+            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+        }
+    }
+}
